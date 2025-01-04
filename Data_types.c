12732e967821155c6fedfff835535eb877012111
vishwashirade10 <officialvishwas1010@gmail.com>
2025-01-04 19:24:49 +0530

    changes in data type

--- Data_types.c
+++ Data_types.c
@@ -2,12 +2,18 @@
 // 1 char
 // 2  int
 // 3 float
-#include<stdio.h>
-int main (){
-     
-  int age=20;
-float p1=3.14;
-char hastag ='#';
+#include <stdio.h>
+int main()
+{
 
-     return 0;
+  int age = 20;
+  float p1 = 3.14;
+  char hastag = '#';
+  printf("Vishwas Hirade\n");
+  printf("%d\n",age);
+  printf("%f\n",p1);
+  printf("%c\n",hastag);
+
+
+  return 0;
 }
