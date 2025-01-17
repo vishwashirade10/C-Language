851614c11cc4918f64dc499a348fa7745fdff701
Vishwas Hirade <officialvishwas1010@gmail.com>
2025-01-17 11:01:07 +0530

    Create function.c

---
+++ function.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+
+    printf("len of str=%d\n", strlen("hello om pawar2 "));
+
+
+char src[100]= "om Anna pawar";
+char dest [100];
+strcpy (dest,src);
+printf("dest=%s\n",dest);
+    return 0;
+}
